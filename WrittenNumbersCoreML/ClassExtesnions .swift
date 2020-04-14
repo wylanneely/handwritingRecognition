2789f5ff212d2785e79736a9d83b64e1b2e0209b
wylanneely <wylanneely@gmail.com>
2020-04-14 02:22:01 -0600

    HandWriter predictor using the camera
    - created PhotoCaptureViewController 
    -capture photo of your writing, then predict character/number
    -refactored controller files, and marked up some comments

---
+++ WrittenNumbersCoreML/ClassExtesnions .swift
@@ -0,0 +1,40 @@
+//
+//  ClassExtesnions .swift
+//  WrittenNumbersCoreML
+//
+//  Created by Wylan L Neely on 4/11/20.
+//  Copyright © 2020 Wylan L Neely. All rights reserved.
+//
+import UIKit
+import Foundation
+
+extension UIImage {
+    
+    convenience init(view: UIView) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: image!.cgImage!)
+    }
+}
+
+extension String {
+    
+    func returnAsInteger() -> Int?{
+        switch self {
+        case "0": return 0
+        case "1": return 1
+        case "2": return 2
+        case "3": return 3
+        case "4": return 4
+        case "5": return 5
+        case "6": return 6
+        case "7": return 7
+        case "8": return 8
+        case "9": return 9
+        default:
+            return nil
+        }
+    }
+}
